40dd28cd991880d6e69a747bffb8fe3e4c8e7b56
qwerty9927 <tanvo9927@gmail.com>
2022-08-13 12:04:38 +0700

    #11 - create feature receive image and save it

--- src/services/Chat.service.js
+++ src/services/Chat.service.js
@@ -1,17 +1,31 @@
+const fs = require("fs")
+const fileType = require('file-type-es5')
 const ChatModel = require("../models/Chat.model")
-
-class ServiceChat{
-  connect(socket){
+const path = '../../public/uploads'
+class ServiceChat {
+  connect(socket) {
     console.log("connect to chat")
     socket.on("entryRoom", (data) => {
-      socket.join("room-" + data.idRoom) 
+      socket.join("room-" + data.idRoom)
     })
 
-    socket.on("sendMessage", async (data) => {
-      try{
-        await ChatModel.addMessage(data.idRoom, data.messageInfo)
+    socket.on("sendMessage", async (data, file, callback) => {
+      try {
+        // await ChatModel.addMessage(data.idRoom, data.messageInfo)
+        const path = __basedir + `\\public\\uploads\\imgs\\${data.idRoom}`
+        const fileInfo = fileType(file)
+        console.log(fileInfo)
+        const filename = Date.now() + '-' + Math.round(Math.random() * 1E9) + '-' + `${data.username}.${fileInfo.ext}`
+        if (!fs.existsSync(path)) {
+          fs.mkdirSync(path)
+        }
+        if (file) {
+          fs.writeFile(path + `\\${filename}`, file, { flag: 'w' }, (err) => {
+            callback({ message: err ? "failure" : "success" })
+          })
+        }
         socket.to("room-" + data.idRoom).emit("receiveMessage", data.messageInfo)
-      } catch(e){
+      } catch (e) {
         console.log(e)
       }
     })
